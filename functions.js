65ba0e32c278d5becd8c71a5c4c48029453d3a11
Shivansh Rawat <shivanshrawat587@gmail.com>
2024-02-29 21:01:33 +0530

    functions added

--- functions.js
+++ functions.js
@@ -7,7 +7,7 @@
 //3. ANONYMOUS FUNCTION: A FUNCTION WITHOUT A NAME.
 //4. ARROW FUNCTION: A FUNCTION WHICH HAVE A SYNTAX AS AN ARROW. ()=>{  }
 //5. IIFE: IMMEDIATELY INVOKED FUNCTION EXPRESSION.
-//6. CALLBACK FUNCTION: A FUNCTION WHICH IS PASSED AS A PARAMETER TO ANOTHER FUNCTION.
+//6. CALLBACK FUNCTION: A FUNCTION WHICH IS PASSED AS A ARGUMENT TO ANOTHER FUNCTION.
 //7. NAMED FUNCTION: A FUNCTION WITH A NAME.
 //8. Higher order function: A function which accepts another function as a parameter.
 
@@ -26,3 +26,45 @@ let abc = declaration(1,2); //argurments= > Actual values. FUNCTION KI RETURN VA
 
 //FUNCTION EXPRESSION
 
+let varFunc = function (a, b) {
+  return a + b;
+}
+
+
+//ANONYMOUS FUNCTION
+
+let anonymous = function () {
+  console.log("I am an anonymous function");
+}
+
+
+//ARROW FUNCTION == released in es6, this function handles this keyword in a better way.
+
+let arrow = (a, b, callback) => {
+console.log("This is arrow funciton")
+callback()
+}
+
+// arrow(1,2,callback);
+
+//IIFE=> Immediately Invoked Function Expression
+
+// (function iife(){
+//   console.log("IIFE function");
+// })();
+
+
+//CALLBACK FUNCTION => These are passed inside a function as argument.
+
+function callback() { 
+  console.log("This is a callback function");
+}
+
+//HIGHER ORDER FUNCTION => A function which accepts another function as a parameter.
+function higherOrderFunction(callback){
+  callback();
+  console.log("This is a higher order function");
+
+}
+
+higherOrderFunction(callback)
