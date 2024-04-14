cd7bfff326cbb69ab697f31ede0cd02d3d0da45b
Shivansh Rawat <shivanshrawat587@gmail.com>
2024-04-14 10:18:31 +0530

    async await

---
+++ async-await/script.js
@@ -0,0 +1,70 @@
+//Async Await is a new way to write asynchronous code. It is built on top of promises, so it is also non-blocking.
+
+// console.log("Start");
+
+// const start2 = () => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve("Start 2");
+//     }, 2000);
+//   });
+// };
+
+// const start3 = () => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve("Start 3");
+//     }, 1000);
+//   });
+// };
+// start2.then((result) => {
+//   console.log(result);
+
+//   start3.then((result) => {
+//     console.log(result);
+//   });
+// });
+
+// console.log("End");
+
+//async = >> function= > Using this keyword you can make a function asynchronous.
+
+//await = >> promise= > Using this keyword you can wait for a promise to be resolved.
+
+// let race = async () => {
+//   console.log("Start");
+//   let startMethod = await start2();
+//   let startMethod2 = await start3();
+//   console.log(startMethod);
+//   console.log(startMethod2);
+//   console.log("End");
+// };
+
+// race();
+let container = document.querySelector(".container");
+let getData = async () => {
+  let data = await fetch("https://reqres.in/api/users?page=1");
+  let response = await data.json();
+  let usersData = response.data;
+//   console.log(response);
+
+          usersData.map((user) => {
+          let html = ` <div class="user">
+            <h1> ${user.first_name + " " + user.last_name} </h1>
+            <h2> ${user.email}</h2>
+            <img src=${user.avatar} alt="">
+            </div>`;
+          container.innerHTML += html;
+        });
+};
+
+getData();
+
+
+// fetch("https://reqres.in/api/users?page=1")
+//     .then((response) => {
+//       response.json().then((data) => {
+//         console.log(data);
+//         console.log(data.data[0]);
+//       })
+//     })
